d8ddb619e1ecb074318c00f38f2fdae8e00e1379
mxerinda <mxerinda@singular.co.za>
2023-02-23 04:50:19 -0800

    Tasks

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -6,10 +6,10 @@
  */
 void print_numbers(void)
 {
-	int num;
+	int i;
 
-	for (num = 0; num <= 9; num++)
-		_putchar((num % 10) + '0');
-
-	_putchar('\n');
+	for (i = 0; i<10; i++)
+	  {
+	    _putchar("%d\n",i);
+}
 }
