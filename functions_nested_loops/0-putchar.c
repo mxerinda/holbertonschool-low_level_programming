3c6770b548017542801b34a951df1adad641831f
mxerinda <mxerinda@singular.co.za>
2023-02-22 22:26:18 -0800

    Task0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -5,7 +5,8 @@
 * Return: Always 0 (Success)
 */
 int main(void)
-{   _putchar('_');
+{
+     _putchar('_');
   _putchar('p');
   _putchar('u');
   _putchar('t');
@@ -14,5 +15,6 @@ int main(void)
   _putchar('a');
   _putchar('r');
   _putchar('\n');
+  
    return (0);
 }
