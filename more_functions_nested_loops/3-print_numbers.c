fbc0a4a2be654b16f249edf6414c8d562d02fd1c
mxerinda <mxerinda@singular.co.za>
2023-02-23 04:54:58 -0800

    Tasks

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,10 @@
-
 #include "main.h"
 
-/**
- * print_numbers - Prints the numbers from 0-9.
- */
 int main()
 {
 	int i;
 
-	for (i = 0; i<10; i++)
+	for(i=0;i<10;i++)
 	  {
 	    _putchar("%d\n",i);
 }
