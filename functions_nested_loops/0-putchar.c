aeb49e3638fb96800fb489d843d561d0dd8e694a
mxerinda <mxerinda@singular.co.za>
2023-02-22 22:18:18 -0800

    Tasks

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 /**
- * main - Entry point
- * Return: Always 0.
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
-{
-	printf("_putchar\n");
-	return (0);
+{   _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar('\n');   return (0);
 }
